9cde19899d899604c3ee6e0e4397bb03553302b6
Makerslet <oshuev.valery@yandex.ru>
2020-03-10 20:23:02 +0300

    Шаблон проекта

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/**
+ * @brief Entry point
+ *
+ * Execution of the program
+ * starts here.
+ *
+ * @return Program exit status
+ */
+
+int main (int, char **)
+{
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
+
+
