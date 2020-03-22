0e127bc49ca51f5564198987fdfb062d21668280
Makerslet <oshuev.valery@yandex.ru>
2020-03-22 13:08:22 +0300

    реализация итератора

--- src/infinite_matrix.h
+++ src/infinite_matrix.h
@@ -26,7 +26,18 @@ class matrix_row : ISizable
 {
     using element = std::pair<std::size_t, T>;
 public:
-    matrix_row(std::size_t row_number) : _row_number(row_number) {}
+
+    matrix_row(std::size_t row_number = 0) : _row_number(row_number) {}
+
+    using elements_iterator = typename std::vector<element>::iterator;
+    elements_iterator begin() {
+        return _elements.begin();
+    }
+
+    elements_iterator end() {
+        return _elements.end();
+    }
+
     std::size_t size() const override {
         return _elements.size();
     }
@@ -102,7 +113,103 @@ class infinite_matrix : ISizable
 {
     using element = std::pair<std::size_t, matrix_row<T, def_value>>;
 public:
+    using elements_iterator = typename std::vector<element>::iterator;
+    using iterator_value = std::tuple<std::size_t, std::size_t, T>;
+
+    class matrix_iterator: public std::iterator<
+            std::forward_iterator_tag, iterator_value>
+    {
+        using matrix_row_iterator = typename matrix_row<T, def_value>::elements_iterator;
+    public:
+        matrix_iterator(elements_iterator ext_iterator,
+                        matrix_row_iterator int_iterator,
+                        elements_iterator end_iterator,
+                        matrix_row_iterator dummy_iterator) :
+            _external_iterator(ext_iterator),
+            _internal_iterator(int_iterator),
+            _end_iterator(end_iterator),
+            _dummy_iterator(dummy_iterator)
+        {}
+
+        matrix_iterator(elements_iterator ext_iterator,
+                        matrix_row_iterator int_iterator) :
+            _external_iterator(ext_iterator),
+            _internal_iterator(int_iterator),
+            _end_iterator(ext_iterator),
+            _dummy_iterator(int_iterator)
+        {}
+
+        matrix_iterator operator++() {
+            matrix_iterator tmp = *this;
+            if(++_internal_iterator == _external_iterator->second.end()) {
+                ++_external_iterator;
+                if(_external_iterator != _end_iterator)
+                    _internal_iterator = _external_iterator->second.begin();
+                else
+                    _internal_iterator = _dummy_iterator;
+            }
+            return tmp;
+        }
+
+        matrix_iterator operator++(int) {
+            if(++_internal_iterator != _external_iterator->end()) {
+                ++_external_iterator;
+                if(_external_iterator != _end_iterator)
+                    _internal_iterator = _external_iterator->begin();
+                else
+                    _internal_iterator = _dummy_iterator;
+            }
+            return *this;
+        }
+        iterator_value operator*() {
+            _current_value = std::make_tuple(_external_iterator->first,
+                                             _internal_iterator->first,
+                                             _internal_iterator->second);
+            return _current_value;
+        }
+        iterator_value* operator->() {
+            return _current_value = std::make_tuple(_external_iterator->first,
+                                                    _internal_iterator->first,
+                                                    _internal_iterator->second);
+            return &_current_value;
+        }
+
+        bool operator==(const matrix_iterator& rhs) {
+            return (_external_iterator == rhs._external_iterator &&
+                    _internal_iterator == rhs._internal_iterator);
+        }
+        bool operator!=(const matrix_iterator& rhs) {
+            return (_external_iterator != rhs._external_iterator ||
+                    _internal_iterator != rhs._internal_iterator);
+        }
+
+
+    private:
+        elements_iterator _external_iterator;
+        matrix_row_iterator _internal_iterator;
+        iterator_value _current_value;
+
+        elements_iterator _end_iterator;
+        matrix_row_iterator _dummy_iterator;
+    };
+
     infinite_matrix(){}
+
+    matrix_iterator begin() {
+        auto it_begin = _elements.begin();
+
+        if(it_begin != _elements.end())
+            return matrix_iterator(it_begin, it_begin->second.begin(), _elements.end(), _dummy.begin());
+        else
+            return matrix_iterator(it_begin, _dummy.begin());
+    }
+
+    matrix_iterator end() {
+        auto it_end = _elements.end();
+        return matrix_iterator(it_end, _dummy.begin());
+    }
+
+
     std::size_t size() const override {
         auto add = [](std::size_t sum, const element& elem){
             return sum + elem.second.size();
@@ -119,6 +226,7 @@ public:
 
 private:
     std::vector<element> _elements;
+    matrix_row<T, def_value> _dummy;
 };
 
 template <typename T, T def_value>
--- src/main.cpp
+++ src/main.cpp
@@ -24,20 +24,16 @@ int main (int, char **)
     assert(matrix[100][100] == 314);
     assert(matrix.size() == 1);
 
-    matrix[100][100] = default_value;
-    assert(matrix[100][100] == default_value);
-    assert(matrix.size() == 0);
-
-//    // выведется одна строка
-//    // 100100314
-//    for(auto c: matrix)
-//    {
-//        int x;
-//        int y;
-//        int v;
-//        std::tie(x, y, v) = c;
-//        std::cout << x << y << v << std::endl;
-//    }
+    // выведется одна строка
+    // 100100314
+    for(auto c: matrix)
+    {
+        int x;
+        int y;
+        int v;
+        std::tie(x, y, v) = c;
+        std::cout << x << y << v << std::endl;
+    }
     return 0;
 }
 
